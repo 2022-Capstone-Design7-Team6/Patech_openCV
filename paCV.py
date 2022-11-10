145bf45ed74476055b76c558a1ed7c2f08cf434f
Dandelioon51 <mbyeongseo@gmail.com>
2022-11-11 03:09:03 +0900

    use triple_mask

--- paCV.py
+++ paCV.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 import sys
-from  PIL  import Image
+import datetime
 
 #카메라 위치 고려하여 길이를 측정 기능?
 
@@ -10,74 +10,84 @@ from  PIL  import Image
 #입력 : 서버 상 이미지 type
 #출력 : ndarray (dtype = uint8)
 def convert2NdArray(img):  #change type to ndarray and dtype is np.uint8  !!!타입을 알아야함
+    if type(img) !=np.ndarray :
+        print("Type is not numpy.ndarray")
     return img
 
-#상태 : 업그레이드 중
+#상태 : 업그레이드 중 두께 가중치 추가?
 #기능 : 파 넓이 계산
-#입력 : image=ndarray , ratio=0~1, potTopCentimeter=cm
-#출력 : 넓이(cm^2), 높이(cm)
-def paPic(img,ratio,potTopCentimeter):#파사진을 찍었을 때 맨위 위치의 위로 파란색부분을 찾아 넓이계산
-    potTopPixel =int(len(img)*ratio)
+#입력 : image=ndarray , pakind=종류(대파=0,쪽파=1,양파=2) ,ratio=0~1, potTopCentimeter=cm
+#출력 : [넓이(cm^2), 높이(cm), 무게(g)]
+def paImg2AHW(img,paType, ratio,potTopCentimeter):#파사진을 찍었을 때 맨위 위치의 위로 파란색부분을 찾아 넓이계산
+    area2weight = [2,1,1]#대파, 쪽파, 양파
+    pxH = len(img)
+    pxW = len(img[0])
+    potTopPixel =int(pxH*ratio)
     #RGB로 특정색을 추출하면 어두운 사진에서 정확도가 떨어짐
     #HSV로 진행. H가 색깔, S가 채도(높으면 선명해짐), V가 명도(낮으면 어두어짐)
-    original = img #출력원할떄..
+    
+    #if you want to see output..1
+    original = img 
+    
     newImg = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     #명도 high case 
-    lower_green = (30, 25, 100)
-    upper_green = (90, 255, 255)
+    lower_green = (30, 25, 25)
+    upper_green = (90, 120, 255)
     green_mask = cv2.inRange(newImg, lower_green, upper_green)
     #채도 high case
-    lower_green = (30, 100, 25)
-    upper_green = (90, 255, 255)
+    lower_green = (30, 25, 25)
+    upper_green = (90, 255, 120)
     green_mask2 = cv2.inRange(newImg, lower_green, upper_green)
-    #여러케이스를 합함
-    green_mask+=green_mask2
-    #top 아래는 모두 0으로 바꿈
-    green_mask[len(img)-potTopPixel:, :]=0
+    #색조 high case
+    lower_green = (90, 50, 120)
+    upper_green = (95, 70, 170)
+    green_mask3 = cv2.inRange(newImg, lower_green, upper_green)
     
-    #if you want to see output..
+    #여러케이스를 합함
+    green_mask=green_mask+green_mask2+green_mask3
+    #top 아래는 모두 0으로 바꿈
+    green_mask[pxH-potTopPixel:, :]=0
+    
+    #if you want to see output..2
     newImg = cv2.bitwise_and(original, original, mask = green_mask)
     cv2.imwrite('result6.png', newImg)
     
-    #calculate area
-    countPixel=np.count_nonzero(green_mask)
-    countAllPixel = len(img)*len(img[0])
+    #calculate area ,height, weight
+    countGreenPixel=0
+    heightRow = 0
+    for row in range(pxH):
+        temp  =np.count_nonzero(green_mask[row])
+        if heightRow==0 and temp >=2: #1 could be not accurate
+            heightRow = row
+        countGreenPixel+=temp
+    countAllPixel = pxH*pxW
     heightCM = potTopCentimeter/ratio
-    widthCM= heightCM*len(img[0])/len(img)
+    widthCM= heightCM*pxW/pxH
     allArea = heightCM*widthCM
+    greenArea = round(allArea*countGreenPixel/countAllPixel,1)
     
-    area = round(countPixel*allArea/countAllPixel,1)
-    for g in range(len(green_mask)):
-        if np.count_nonzero(green_mask[g])>=2:
-            heightpx = len(green_mask)-g
-            break
-    heightpx = heightpx - int(ratio*len(img))
-    height = round(heightpx*heightCM/len(img),1)
+    heightRows = pxH - heightRow-int(ratio*pxH)
+    height = round(heightCM*heightRows/pxH,1)
     
-    return area,height
+    weight = round(greenArea*area2weight[paType],1)
+    return [greenArea,height,weight]
     
 #상태 : 구현완료
 #기능 : 두 이미지 파 넓이 차이 계산
 #입력 : before_image=ndarray , after_image=ndarray , ratio=0~1, potTopCentimeter=cm
-#출력 : 넓이(cm^2)
-def paHarv(before_img,after_img,ratio, potTopCentimeter):#수확시, 두 파사진이 동시에 왔을 때 차를 반환 완료
-    areaDiff= paPic(after_img,ratio, potTopCentimeter)-paPic(before_img,ratio, potTopCentimeter) 
-    if areaDiff<=0 :
+#출력 : 두 이미지 [넓이(cm^2), 높이(cm), 무게(g)] 의 차
+def paHarvest(before_img,after_img,paType,ratio, potTopCentimeter):#수확시, 두 파사진이 동시에 왔을 때 차를 반환 완료
+    diff= [round(a - b,1) for a, b in zip(paImg2AHW(after_img,paType, ratio, potTopCentimeter), paImg2AHW(before_img,paType,ratio, potTopCentimeter) )]
+    if diff[0]<0 :
         return 'ERROR, pa is grown..'
     else :
-        return areaDiff
+        return diff
     
-#상태 : 구현 전 평균값 가져와야함
-#기능 : 파의 넓이를 무게로 환산
-#입력 : pakind=종류(대파=0,쪽파=1,양파=2) area=넓이(cm^2)
-#출력 : 무게(g)
-def area2weight(pakind,area):
-    cm2weight = [2,1,1]
-    if pakind ==0 or pakind ==1 or pakind ==2:
-        return area * cm2weight[pakind]
-    else :
-        print("ERROR: wrond pa ID")
-        return -1
-    
-def harvExp(heights):
-    pass
+#상태 : 구현전
+#기능 : 성장 곡선 예측, 수확시기 예측
+#입력 : heightList = [[datetime1,height1],[datetime2,height2],[datetime3,height3]...]
+#출력 : 수확 시기...?
+def harvPredict(heightList):
+    #[[datatime,50]]
+
+    return True
