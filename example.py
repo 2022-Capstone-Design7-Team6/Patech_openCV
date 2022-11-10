a8d5a188cb971667dffb0a0a60668237263d5c06
Dandelioon51 <mbyeongseo@gmail.com>
2022-11-11 03:10:51 +0900

    added guide code

--- example.py
+++ example.py
@@ -1,24 +1,36 @@
 import sys
 import paCV
 import cv2
-#example user data
+import numpy
+import datetime
+
+#-----EXAMPLE USER DATA-----#
+#example user database
 class user:
-    def __init__(self, ratio, potTopCentimeter):
+    def __init__(self, ratio, potTopCentimeter,heightList):
         self.ratio =ratio
         self.potTopCentimeter= potTopCentimeter
-client1 = user(0.45,20)
+        self.heightList= heightList
+client1 = user(0.45,20,[[datetime.date(2022,11,1),0],[datetime.date(2022,11,30),10]])
+#example img data
+img = cv2.imread("C:/Users/minby/Desktop/codes/capstone/H2.jpg")
+img2 = cv2.imread("C:/Users/minby/Desktop/codes/capstone/G2.jpg")
 
 
-#type of img is numpy.ndarray
-img = cv2.imread("C:/Users/minby/Desktop/pictures/pas/p10_10/G1.jpg")
+
+#-----GUIDE CODE-----#
+#def convert2NdArray(img=server image type)
 img = paCV.convert2NdArray(img)
+print('Image type is converted to ',type(img))
 
+#def paImg2AHW(img,paType, ratio,potTopCentimeter)
+output = paCV.paImg2AHW(img,0,client1.ratio,client1.potTopCentimeter)
+print('Output of paImg2AHW is List : ',output,'(unit is cm^2, cm, g)')
 
-#img_trans = paCV.picTrans(img)
+#def paHarvest(before_img,after_img,ratio, potTopCentimeter)
+output = paCV.paHarvest(img,img2,0,client1.ratio,client1.potTopCentimeter)
+print('Output of paHarvest is List : ',output,'(unit is cm^2, cm, g)')
 
-
-#using paPic
-img = cv2.imread("C:/Users/minby/Desktop/codes/capstone/G1.jpg")
-print(paCV.paPic(img,client1.ratio,client1.potTopCentimeter))
-
-print(paCV.area2weight(0,20))
+#def harvPredict(heightList)
+output = paCV.harvPredict(client1.heightList)
+print('Output of harvPredict : ',output)
